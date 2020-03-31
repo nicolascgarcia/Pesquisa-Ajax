15c2bd7ce015a11693e2f744fd016c618663f6f8
Nicolas Garcia <ni.car.garcia@gmail.com>
2020-03-31 00:23:35 -0300

    remocao do banco servidor

--- backend/conecta.php
+++ backend/conecta.php
@@ -3,7 +3,6 @@
 //===========conexão com banco de dados =============
 
 $conn = new mysqli("localhost","root","","pesquisaajax"); //banco localhost
-//$conn = new mysqli("localhost","leoimo18_admin","qawsedrf","leoimo18_test"); //banco servidor
 $fuso = new DateTimeZone('America/Sao_Paulo');
 $data = new DateTime();
 $data->setTimezone($fuso);
